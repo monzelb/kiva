e24c3fcda780d33e32c24b43ea79c5ec961b8d2a
Brett Monzel <monzelb@gmail.com>
2018-10-24 14:43:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -42,20 +42,6 @@
   ?>
   <body>
 
-  sdafsdfasfsdf
-  <h1>Realtime communication with WebRTC</h1>
-
-  <video id="localVideo" autoplay></video>
-  <video id="remoteVideo" autoplay></video>
-
-  <div>
-    <button id="startButton">Start</button>
-    <button id="callButton">Call</button>
-    <button id="hangupButton">Hang Up</button>
-  </div>
-
-  <script src="js/lib/adapter.js"></script>
-  <script src="js/main.js"></script>
 
   
   <div class = "header">
